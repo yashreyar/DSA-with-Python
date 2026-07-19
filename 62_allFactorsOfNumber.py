3bd1c9c0bcbfe69e9e059b0e65c73f5517cb146f
iyashreyar <yashreyar5143@gmail.com>
2026-07-19 14:23:37 +0530

    DSA ques solved

--- 62_allFactorsOfNumber.py
+++ 62_allFactorsOfNumber.py
@@ -5,6 +5,19 @@ Input = 10
 Output = [1,2,5,10]
 '''
 
+from math import *
+def are_factors(n):
+    lst = []
+    limit = int(sqrt(n))
+    for i in range(1, limit+1):
+        if n % i == 0:
+            lst.append(i)
+            if i != n // i:
+                lst.append(n//i)
+    return sorted(lst)
+
+print(are_factors(n=50))
+'''
 def are_factors(n):
     lst = []
     if n <= 0:
@@ -16,6 +29,9 @@ def are_factors(n):
     return lst
 
 print(are_factors(n=1))
+'''
+
+# OR
 
 '''
 def are_factors(n):

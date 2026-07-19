77507f0f7498121b22c67891b8b1c6106bdf9eeb
iyashreyar <yashreyar5143@gmail.com>
2026-07-19 14:52:05 +0530

    DSA ques solved

--- 63_countNoFrequency.py
+++ 63_countNoFrequency.py
@@ -1,4 +1,16 @@
+# Using HashMap
+def count_number_freq(lst):
+    hash_map = {}
+    n = len(lst)
+    for i in range(n):
+        hash_map[lst[i]] = hash_map.get(lst[i], 0) + 1
+    return hash_map
+
+print(count_number_freq(lst = [1,3,2,4,5,3,1,2,4,2]))
+# OR
+
 # O(n)
+'''
 def count_numbers_frequency(lst):         
     result = {}
     for num in lst:                 # O(n)
@@ -9,3 +21,4 @@ def count_numbers_frequency(lst):
     return result
 
 print(count_numbers_frequency(lst=[1,2,5,4,3,2,52,1,4,1,2,5,3]))
+'''

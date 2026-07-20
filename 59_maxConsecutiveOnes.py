0ae5e913b3018515d6ec8932284db43b92d5f9c2
iyashreyar <yashreyar5143@gmail.com>
2026-07-21 01:13:43 +0530

    DSA ques solved

--- 59_maxConsecutiveOnes.py
+++ 59_maxConsecutiveOnes.py
@@ -3,13 +3,15 @@ Input: nums = [1, 0, 1, 1, 0, 1, 1, 1, 1]
 Output: 4
 '''
 
+# Time complexity: O(n)
+# Space complexity: O(1)
 def find_max_consecutive_ones(nums):
     count = 0
     max_count = 0
-    for num in nums:
+    for num in nums:                                # time: O(n)
         if num == 1:
             count += 1
-            max_count = max(count, max_count)
+            max_count = max(count, max_count)       # time: O(1)
         else:
             count = 0
     return max_count
